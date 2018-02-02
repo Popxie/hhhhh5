09cb8d802da6556619818f2f42896949c1f963bd
kaka <itxiehuaqiang@163.com>
2018-02-02 09:47:35 +0800

    fix bugs  恢复误删除的微信延签

--- src/views/Station/js/stationDetails.js
+++ src/views/Station/js/stationDetails.js
@@ -23,6 +23,7 @@ new Vue({
                 !res.data.data.imgUrl ? res.data.data.imgUrl = 'http://p1lw91kqi.bkt.clouddn.com/defaultPic.jpg' : '';
                 this.stationInfo = Object.assign({}, this.stationInfo, res.data.data);
             })
+            this.getWeixinJsapiTicket();
     },
     methods: {
         transferFn(data) {
@@ -32,6 +33,38 @@ new Vue({
                 result += encodeURIComponent(i) + '=' + encodeURIComponent(data[i]) + '&'
             }
             return result
+        },
+        getWeixinJsapiTicket() {
+            var self = this;
+            var url = "https://mgoapi.18jian.cn/api-account-web/wechat/driver/ticket/jsapi";
+            self.$http.get(url)
+                .then((res) => {
+                    if (res.data.err_msg === 'success') {
+                        var signArgs = {
+                            timestamp: (new Date()).getTime(),  // 时间戳
+                            noncestr: Math.random().toString(36).substr(2), // 随机字符串
+                            jsApiTicket: res.data.data.ticket,              // jsApiTicket
+                        };
+
+                        let url = location.href;
+                        let preSha = 'jsapi_ticket=' + signArgs.jsApiTicket + '&noncestr=' + signArgs.noncestr + '&timestamp=' + signArgs.timestamp + '&url=' + url;
+                        let signResult = {
+                            noncestr: signArgs.noncestr,
+                            timestamp: signArgs.timestamp,
+                            signature: sha1(preSha)
+                        }
+                        wx.config({
+                            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                            appId: "wxd4c744355b802aec",
+                            timestamp: signResult.timestamp,
+                            nonceStr: signResult.noncestr,
+                            signature: signResult.signature,
+                            jsApiList: self.jsApiList
+                        });
+                    }
+                }).catch((err) => {
+                    console.log('err', err);
+                });
         },
         mapClick() {
             // 使用微信内置地图查看位置接口
